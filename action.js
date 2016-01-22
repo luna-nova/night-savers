18fb4fde908478c485aefe4823bbba810fc58cd7
luna-nova <heartofnova@gmail.com>
2016-01-22 10:58:53 -0800

    ready for music and code sections animations now

--- action.js
+++ action.js
@@ -19,8 +19,11 @@ $(document).ready(function() {
         "-ms-flex-wrap": "nowrap",
         "flex-wrap": "nowrap"
       }).animate({}, 1000, function() {
-        $("#music").fadeIn()
-        $("#code").fadeIn()
+        $("#music").fadeIn("slow", function() {
+          $(".main-section").css({"background-color": "rgba(150, 150, 150, 0.50)"});
+          $("#main-strip").css({"background-color": "rgba(0, 0, 0, 0)"});
+        });
+        $("#code").fadeIn();
       })
     });
   });
